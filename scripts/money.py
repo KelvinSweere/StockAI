286a37fe28ac0c04f8391ec8e1916de7b6de32e3
DESKTOP-0KLUDVO\Kelvin <kelvinswe96@hotmail.com>
2020-10-20 17:35:33 +0200

    Updated money class for crypto with float values as parameters.

--- scripts/money.py
+++ scripts/money.py
@@ -11,7 +11,7 @@ class InvestmentPortfolio():
         self.printBool = False
         self.TAKER_FEE = 0.26 / 100  #0.16% KRAKEN
 
-    def buy(self, price):
+    def buy_whole_stocks(self, price):
         """Buy stocks with price per stock.
 
         Args:
@@ -34,26 +34,78 @@ class InvestmentPortfolio():
 
         if(self.printBool):
             self.printValues()
+        
+    def buy(self, price, num):
+        """Buy stock with price per stock and num. 
+
+        Args:
+            price (float): price per stock
+            num (float): number of stocks (can be float value)
+        """
+        total_stock_price = price * num
+        fee_price = round(total_stock_price * self.TAKER_FEE,2)
+
+        spending_money = total_stock_price + fee_price  
+        
+        print(str(spending_money) + " = " + str(total_stock_price) + " + " + str(round(fee_price,2)))
+
+        if(spending_money > self.cash): 
+            print("Can't be bought! Adjust number of stocks.")     
+        else:
+            self.cash -= round(fee_price,2)
+            self.cash -= price * num
+            self.stock_num += num
+
+            print("stock num = " + str(self.stock_num))
+            print("cash = " + str(self.cash))
 
 
-    def sell(self, price):
+
+    def sell_all(self, price):
         """Sell all stocks with price parameter.
 
         Args:
             price (float): Price per stock.
         """
-        print(str(int(self.stock_num)) +
-              " stocks sold with a price of €" + str(price))
-        self.cash += price * self.stock_num
-        self.stock_money = 0 #sell all.
-        self.stock_num = 0  #reset stocks.
+        round(price,2)
+
+        print(str(round(self.stock_num, 4)) +
+            " stocks sold with a price of €" + str(price) + " per coin")
+
+        self.cash += price * self.stock_num - round(price * self.stock_num * self.TAKER_FEE, 2) 
+        self.stock_money = 0.0 #sell all.
+        self.stock_num = 0.0  #reset stocks.
 
         if(self.printBool):
-            self.printValues()
+            self.print_values()
         
         print("Cash status = " + str(self.cash))
 
-    def printReturn(self):
+    def sell(self, price, num):
+        """Sell stocks with price and number of stocks.
+
+        Args:
+            price (float): price of stocks.
+            num (float): number of stocks (can be float value with currency).
+        """
+        if(num > self.stock_num):
+            print("Invalid number. Change number of stocks to sell.")
+        else:
+            print(str(round(self.stock_num, 4)) +
+                " stocks sold with a price of €" + str(price) + " per coin")
+
+            fund_to_cash = price * num - round(price * num * self.TAKER_FEE, 2) 
+
+            self.cash += fund_to_cash
+
+            self.stock_num -= num  
+            self.stock_money = price * self.stock_num   
+
+            if(self.printBool):
+                self.print_values()
+            print("Cash status = " + str(self.cash))
+
+    def print_return(self):
         """Print return of investment in %.
 
         Returns:
@@ -64,7 +116,7 @@ class InvestmentPortfolio():
 
         print("Current return value = " + str(round(returns, 2)) + "%")
 
-    def printValues(self):
+    def print_values(self):
         """Print all important values.
 
         Returns:
@@ -75,7 +127,7 @@ class InvestmentPortfolio():
         print("With a stock value of = " + str(self.stock_money))
         print("\n")
 
-    def depositCash(self, cash):
+    def deposit_cash(self, cash):
         """Send cash to current portfolio.
 
         Args:
@@ -85,7 +137,7 @@ class InvestmentPortfolio():
         self.paid_up_cap += cash
         print("New cash status = " + str(self.cash))    
 
-    def takeCash(self, cash):
+    def withdraw_cash(self, cash):
         """Get cash from current portfolio
 
         Args:
@@ -98,15 +150,22 @@ class InvestmentPortfolio():
             print("New cash status = " + str(self.cash))
 
 if __name__ == "__main__":
-    #riple
-    rpl = InvestmentPortfolio()
+    eth = InvestmentPortfolio()
 
-    rpl.buy(0.12278)    #buy date = 12 mar
-    rpl.sell(0.20880)   #sell date = 29 apr
+    # eth.buy(price=312.46, num=0.09547)    #buy date = 20 oct
+    eth.buy(price=293.28, num=0.1010)    #buy date = 20 oct
 
-    rpl.buy(0.15582)    #buy date = 27 jun
-    rpl.sell(0.19118)   #sell date = 27 jul
 
-    rpl.printReturn()
+    # eth.sell_all(325.20)   #sell date = 29 apr
+    eth.sell(price=297.74, num=float(eth.stock_num/2))
+    eth.sell(price=301.75, num=float(eth.stock_num/2))
 
-    print("cash = ", rpl.cash)
+    """
+    eth.buy(0.15582, 100)    #buy date = 27 jun
+    eth.sell(0.19118)   #sell date = 27 jul
+    """
+
+    eth.print_return()
+
+    print("cash = ", eth.cash)
+    
